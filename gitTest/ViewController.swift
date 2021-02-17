e8b7fd59ce9affa5a2532f54fdc79a4cead9a8ff
廖昱晴 <pulin.liao@ctbcsec.com>
2021-02-17 15:50:25 +0800

    add message

--- gitTest/ViewController.swift
+++ gitTest/ViewController.swift
@@ -9,8 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let message = "GitTest"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(message)
         // Do any additional setup after loading the view.
     }
 
